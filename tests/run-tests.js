bc99198d10fc2fb2b6c59cc09cc663e2bf2587d8
weidagang <weidagang@gmail.com>
2015-05-17 17:38:39 -0700

    Add test-callbackless.js

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -1,2 +1,3 @@
+require('./test-callbackless.js');
 require('./test-callbackless-str.js');
 require('./test-callbackless-fs.js');
---
+++ tests/test-callbackless.js
@@ -0,0 +1,46 @@
+var assert = require('assert');
+
+// import the core APIs from the callbackless module
+var cbs = require('../callbackless.js');
+var unit = cbs.unit;
+var fmap = cbs.fmap;
+var liftA = cbs.liftA;
+var flatMap = cbs.flatMap;
+
+// import the testing utils
+var cbs_testing = require('../callbackless-testing.js');
+var print$ = cbs_testing.print$;
+var assertEquals$ = cbs_testing.assertEquals$;
+
+// test cases
+
+function testPromise_fmap() {
+  console.log("Running " + arguments.callee.name);
+
+  var square = function (x) { return x * x; };
+  var square$ = fmap(square);
+  
+  var squareOf10 = square(10);
+  var squareOf10$ = square$(unit(10));
+
+  assertEquals$(unit(squareOf10), squareOf10$);
+}
+
+function testPromise_liftA() {
+  console.log("Running " + arguments.callee.name);
+
+  var squareSum = function (x, y) { return x * x + y * y; };
+  var squareSum$ = liftA(squareSum);
+  
+  var squareSumOf2And3 = squareSum(2, 3);
+  var squareSumOf2And3$ = squareSum$(unit(2), unit(3));
+
+  assertEquals$(unit(squareSumOf2And3), squareSumOf2And3$);
+}
+
+function runTests() {
+  testPromise_fmap();
+  testPromise_liftA();
+}
+
+runTests();
